30bfdf8491e3880531fbba9c14094105b058c99e
Hennadii Kashtymov <kashtymov.eto2@gmail.com>
2023-09-28 18:49:16 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hello");
+var myFunc = function (x) {
+    return x * 2;
+};
+console.log(myFunc(2));
---
+++ index.ts
@@ -0,0 +1,9 @@
+console.log("hello");
+
+const myFunc = (x: number) => {
+    return "hello";
+};
+
+console.log(myFunc(2));
+
+export {};
